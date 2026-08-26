639fe9501cc326a67287df07edc260d0c9c26d65
Nafi Berkay Şahin <olinafiberkay@hotmail.com>
2026-08-26 15:02:08 +0300

    Login Ekranı: form alanları ve validasyon eklendi

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Bu alan boş olamaz';
+                  } else if (!value.contains('@')) {
+                    return 'Geçersiz e-posta girdiniz';
+                  }
+                  return null;
+                },
+
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'E-posta'),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Şifre boş olamaz';
+                  } else if (value.length < 6) {
+                    return 'Şifreniz en az 6 karakter uzunluğunda olmalıdır';
+                  }
+                  return null;
+                },
+
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Şifre'),
+                obscureText: true,
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  if(formKey.currentState!.validate()){
+                    print('Form geçerli! E-posta: ${emailController.text}');
+                  }
+                },
+                child: Text('Giriş Yap'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
